a681f8aa1e3fa953f220227623f1745f44e161ce
Jon Laboy <jonlaboyoc@gmail.com>
2021-02-08 01:33:01 -0500

    Updates

---
+++ precache-manifest.53b453303f0cae0f3a9118b769028c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c56664bb0374026ec26a6c71dc3cf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20c3731ef3afd62fef1b",
+    "url": "/static/css/main.83b7c81f.chunk.css"
+  },
+  {
+    "revision": "0e8c29b1d1642af8df24",
+    "url": "/static/js/2.aaeec303.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aaeec303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c3731ef3afd62fef1b",
+    "url": "/static/js/main.6f051d19.chunk.js"
+  },
+  {
+    "revision": "d516644a39492de2acae",
+    "url": "/static/js/runtime-main.4cb750c5.js"
+  }
+]);
