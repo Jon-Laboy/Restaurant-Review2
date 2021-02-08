32a8ccb006128b27fc5f3ecf177d5747b35ad604
Jon Laboy <jonlaboyoc@gmail.com>
2021-02-08 01:58:58 -0500

    Updates

---
+++ precache-manifest.ba745db39da6dbb765edde7ddc4fd0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca5c0ce47224a50e6eced89df1a9028",
+    "url": "/Restaurant-Review2/index.html"
+  },
+  {
+    "revision": "06fce86c6b964d55a388",
+    "url": "/Restaurant-Review2/static/css/main.83b7c81f.chunk.css"
+  },
+  {
+    "revision": "0e8c29b1d1642af8df24",
+    "url": "/Restaurant-Review2/static/js/2.aaeec303.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Restaurant-Review2/static/js/2.aaeec303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fce86c6b964d55a388",
+    "url": "/Restaurant-Review2/static/js/main.11a73c9b.chunk.js"
+  },
+  {
+    "revision": "d12eee1b8465da5345d4",
+    "url": "/Restaurant-Review2/static/js/runtime-main.bf18feb8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.926807a1755b08b0dc0f514120cf9d4c.js"
+  "/Restaurant-Review2/precache-manifest.ba745db39da6dbb765edde7ddc4fd0e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Restaurant-Review2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
